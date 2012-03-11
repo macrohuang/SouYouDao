40f098a89170eab2e28a443d19674a0814d4144d
Roy Guo <royguo1988@gmail.com>
2012-03-11 19:23:27 +0800

    #

---
+++ app/controllers/RoadMaps.java
@@ -0,0 +1,14 @@
+package controllers;
+
+import play.mvc.Controller;
+
+public class RoadMaps extends Controller {
+
+    public static void index() {
+        render();
+    }
+
+    public static void list(){
+      render();
+    }
+}
---
+++ app/models/RoadMap.java
@@ -0,0 +1,11 @@
+package models;
+
+import play.db.jpa.Model;
+/**
+ * 路线图
+ * @author royguo1988@gmail.com
+ *
+ */
+public class RoadMap extends Model {
+
+}
