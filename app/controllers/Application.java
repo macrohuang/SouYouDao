6a6004edf6d12a50bb1f7becaec68bf4dcb6cebd
Roy Guo <royguo1988@gmail.com>
2012-03-19 22:28:46 +0800

    #

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,5 +1,6 @@
 package controllers;
 
+import models.Admin;
 import play.mvc.Before;
 import play.mvc.Controller;
 import utils.Constants;
@@ -24,7 +25,9 @@ public class Application extends Controller {
       Secure secure = getActionAnnotation(Secure.class);
       //如果没有注解或者注解需要登录均需验证登录状态
       if(secure != null && secure.admin() == true){
-       if(session.get(Constants.ADMIN_ID_IN_SESSION)==null){
+        Long admin_id = Long.parseLong(session.get(Constants.ADMIN_ID_IN_SESSION));
+        Admin admin = Admin.findById(admin_id);
+       if(admin==null){
          Admins.login();
        }
       }
