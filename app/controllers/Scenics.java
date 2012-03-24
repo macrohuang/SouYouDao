8073e5eb5abc8dee94aa5ac596ab76bd6eddf61b
kuankuan <kuankuan@google.com>
2012-03-24 23:55:55 +0800

    #

--- app/controllers/Scenics.java
+++ app/controllers/Scenics.java
@@ -9,6 +9,7 @@ import play.Logger;
 import play.mvc.Controller;
 import utils.Constants;
 import utils.FileUtil;
+import utils.QuerySpliter;
 
 public class Scenics extends Controller {
 
@@ -23,14 +24,19 @@ public class Scenics extends Controller {
 
   public static void search(String keywords, int page) {
     Logger.info("搜索景区：" + keywords);
-    // String[] words = QuerySpliter.splite(keywords);
-    int totalPage =
-        Integer.parseInt(Scenic.count("name like ? order by name asc", "%" + keywords + "%") + "");
+    String[] words = QuerySpliter.splite(keywords);
+    StringBuilder query = new StringBuilder();
+    for (int i = 0; i < words.length; i++) {
+      query.append("name like '%" + words[i] + "%'");
+      if (i < words.length - 1) {
+        query.append(" or ");
+      }
+    }
+    int totalPage = Integer.parseInt(Scenic.count(query + " order by name asc") + "");
     if (page < 1) page = 1;
     if (page > totalPage) page = totalPage;
     List<Scenic> scenics =
-        Scenic.find("name like ? order by name asc", "%" + keywords + "%").fetch(page,
-            Constants.SCENIC_SEARCH_PAGE_SIZE);
+        Scenic.find(query + "order by name asc").fetch(page, Constants.SCENIC_SEARCH_PAGE_SIZE);
     render("Scenics/list.html", scenics, keywords);
   }
 
