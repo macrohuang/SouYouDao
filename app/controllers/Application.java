0f42c796959ddd7fadf37c28227ef2adeaa26e5d
kuankuan <kuankuan@google.com>
2012-03-24 12:09:34 +0800

    #

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -8,28 +8,30 @@ import utils.Secure;
 
 public class Application extends Controller {
 
-    public static void index() {
-      render();
-    }
+  public static void index() {
+    render();
+  }
 
-    public static void search(String keywords){
-      render(keywords);
-    }
+  public static void search(String keywords) {
+    render(keywords);
+  }
 
-    @Before
-    public static void login(){
+  @Before
+  public static void login() {
 
-    }
-    @Before
-    public static void admin(){
-      Secure secure = getActionAnnotation(Secure.class);
-      //如果没有注解或者注解需要登录均需验证登录状态
-      if(secure != null && secure.admin() == true){
-        Long admin_id = Long.parseLong(session.get(Constants.ADMIN_ID_IN_SESSION));
-        Admin admin = Admin.findById(admin_id);
-       if(admin==null){
-         Admins.login();
-       }
+  }
+
+  @Before
+  public static void admin() {
+    Secure secure = getActionAnnotation(Secure.class);
+    // 如果没有注解或者注解需要登录均需验证登录状态
+    if (secure != null && secure.admin() == true
+        && session.get(Constants.ADMIN_ID_IN_SESSION) != null) {
+      Long admin_id = Long.parseLong(session.get(Constants.ADMIN_ID_IN_SESSION));
+      Admin admin = Admin.findById(admin_id);
+      if (admin == null) {
+        Admins.login();
       }
     }
+  }
 }
