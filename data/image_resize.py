20438ff4107de32361825bdfafd677535e9a375b
Roy Guo <royguo1988@gmail.com>
2012-03-29 17:02:37 +0800

    #

---
+++ data/image_resize.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+import glob
+import Image
+
+count = 1;
+for name in glob.glob('scenic/images/*'):
+  try:
+    i = Image.open(name)
+    box = i.getbbox()
+    new_box = (box[0],box[1],box[2],box[3]-50)
+    new_img = i.crop(new_box)
+    new_img.save(name)
+    new_img.thumbnail((128,128))
+    new_img.save(name.replace(r'scenic/images/','scenic/images/thumbs/'),'jpeg')
+    print count,' ',name
+  except Exception as e:
+    print name,'--',e
+  count = count + 1
