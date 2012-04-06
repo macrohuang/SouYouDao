0c448a67fab2fd324cda4a04dad563f7579eeeb1
Roy Guo <royguo1988@gmail.com>
2012-04-06 22:27:44 +0800

    #

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,6 +1,21 @@
 package controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import models.Admin;
+import models.Scenic;
+
+import org.apache.lucene.document.Document;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.Query;
+import org.apache.lucene.search.ScoreDoc;
+import org.apache.lucene.search.TopDocs;
+import org.apache.lucene.store.Directory;
+import org.wltea.analyzer.lucene.IKQueryParser;
+import org.wltea.analyzer.lucene.IKSimilarity;
+
+import play.Logger;
 import play.mvc.Before;
 import play.mvc.Controller;
 import utils.Constants;
@@ -13,7 +28,37 @@ public class Application extends Controller {
   }
 
   public static void search(String keywords) {
-    render(keywords);
+    List<Scenic> scenics = new ArrayList<Scenic>();
+    Logger.info("首页搜索：" + keywords);
+    Directory directory = null;
+    IndexSearcher searcher = null;
+    try {
+      directory = Constants.OPEN_SCENIC_INDEX_DIR();
+      searcher = Constants.CREATE_INDEX_SEARCHER(directory);
+      Query query =
+          IKQueryParser.parseMultiField(new String[] {"name", "description", "address", "tel",
+              "province.name", "city.name"}, keywords);
+      searcher.setSimilarity(new IKSimilarity());
+      // 执行检索操作，根据每次查询的页数取topN
+      TopDocs results = searcher.search(query, 5);
+      ScoreDoc[] scoreDocs = results.scoreDocs;
+      for (ScoreDoc scoreDoc : scoreDocs) {
+        Document document = searcher.doc(scoreDoc.doc);
+        Scenic scenic = Scenic.findById(Long.parseLong(document.get("id")));
+        if (scenic != null) {
+          scenics.add(scenic);
+        }
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
+    } finally {
+      try {
+        if (searcher != null) searcher.close();
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
+    }
+    render(keywords, scenics);
   }
 
   @Before
---
+++ app/controllers/WebSearch.java
@@ -0,0 +1,14 @@
+package controllers;
+
+import play.mvc.Controller;
+
+public class WebSearch extends Controller {
+  /**
+   * 搜索互联网
+   * @param keywords
+   * @param page
+   */
+  public static void search(String keywords, int page) {
+
+  }
+}
