fa3ae36d4f8739fc36a404fb5b28123a3f716829
Roy Guo <royguo1988@gmail.com>
2012-04-28 10:02:44 +0800

    添加Google Analytics

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -64,3 +64,14 @@ function checkSearhBox(){
 		return false;
 	}
 }
+// Google Analytics Start
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-31258415-1']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
+//Google Analytics End
