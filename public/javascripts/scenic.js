3ab2984b5f1f200113ea3bbf6013527e415253c3
macrohuang <macrohuang@126.com>
2012-05-10 20:35:20 +0800

    aa

--- public/javascripts/scenic.js
+++ public/javascripts/scenic.js
@@ -29,7 +29,6 @@ $(function() {
 	initDropdown();
 	tinyMCE.init({
 		mode : "textareas",
-		theme : "advanced",
 		theme_advanced_toolbar_location : "top",
         theme_advanced_toolbar_align : "left",
         theme_advanced_statusbar_location : "bottom",
